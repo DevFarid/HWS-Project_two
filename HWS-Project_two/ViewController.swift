6e3b2b75766a01c2618333a1ec29ddcf2df363a2
Farid <kamizi.3s@osu.edu>
2021-06-30 12:50:06 -0400

    Initial Commit

---
+++ HWS-Project_two/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWS-Project_two
+//
+//  Created by Seyyedfarid Kamizi on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
